7fc9444a7fe061f329b7c2470f19bd4b979aab5b
Craig Boucher <crbouch@microsoft.com>
2018-02-25 15:33:55 -0800

    First draft

---
+++ test/ExpressionDecompiler.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ExpressionDecompiler.Tests
+{
+    public class DecompilingTests
+    {
+        [Fact]
+        public void Decompile()
+        {
+
+        }
+    }
+}
